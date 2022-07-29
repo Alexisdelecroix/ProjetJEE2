884b48d3764416b660371fcd09b47c4310ef0beb
59013-36-15 <d-alexis@hotmail.fr>
2022-07-29 15:46:24 +0200

    Contruction de la première vue, affichage des patients

--- src/main/java/com/example/demo/repositories/PatientRepository.java
+++ src/main/java/com/example/demo/repositories/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repositories;
 
 import com.example.demo.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains (String kw, Pageable pageable);
 }
