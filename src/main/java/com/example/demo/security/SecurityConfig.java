1427d2c3e06c9853884178a56b112725415eb856
59013-36-15 <d-alexis@hotmail.fr>
2022-08-18 12:01:53 +0200

    MAJ

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
 @EnableWebSecurity
 
@@ -12,15 +16,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("USER");
-        auth.inMemoryAuthentication().withUser("user2").password("{noop}12345").roles("USER");
-        auth.inMemoryAuthentication()  .withUser("admin").password("{noop}1234").roles("USER", "ADMIN");
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        String encodedPWS=passwordEncoder.encode("1234");
+        System.out.println(encodedPWS);
+        auth.inMemoryAuthentication().withUser("user1").password(encodedPWS).roles("USER");
+        auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1111")).roles("USER");
+        auth.inMemoryAuthentication()  .withUser("Alexis").password(passwordEncoder.encode("2345")).roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin();
         http.authorizeHttpRequests().anyRequest().authenticated();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
